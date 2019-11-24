75b2012d588e0eb1c961889bea98f3a0747b4446
Joni Parpala <parpala.joni@gmail.com>
2019-11-24 18:43:11 +0200

    muutoksia

--- ui/src/components/ClubsView.js
+++ ui/src/components/ClubsView.js
@@ -7,7 +7,7 @@ const ClubsView = ({ clubs, members }) => {
 
   const [clubMembers, setClubMembers] = useState([])
 
-  const rows = () => clubs.map( club => {
+  /* const rows = () => clubs.map( club => {
 
     const clubMembers = members.filter(member => member.clubId === club.id)
 
@@ -20,27 +20,33 @@ const ClubsView = ({ clubs, members }) => {
       {memberRows()}
       </div>
     )
-  })
+  }) */
 
-  /* const showClubsAndMembers = () => clubs.map( async club => {
+  const showClubMembers = () => clubs.map( club => {
 
-    let clubMembers = await clubService.getClubMembers(club.id)
-    console.log(clubMembers)
-    
+    clubService
+      .getClubMembers(club.id)
+      .then(response => {
+        setClubMembers(response)
+      }).catch(error => console.log(error))
+
+  const memberRows = () => clubMembers.map(member => <li>{member}</li>)
 
     return(
-    <div>
-      <h3>{club.name}</h3>
-      {clubMembers}
-    </div>
+      <div>
+        <h3>{club.name}</h3>
+        {memberRows()}
+      </div>
     )
-  }) */
+    
+  })
 
   return(
 
     <div>
       <h2>Clubs and their members</h2>
-      {rows()}
+      {/* rows() */}
+      {showClubMembers()}
     </div>
 
   )
