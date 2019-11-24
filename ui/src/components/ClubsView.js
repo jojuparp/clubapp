886602e5faabb50f5f3e8a9a9124c88ab3a49be6
Joni Parpala <parpala.joni@gmail.com>
2019-11-24 19:02:18 +0200

    valmiina

--- ui/src/components/ClubsView.js
+++ ui/src/components/ClubsView.js
@@ -1,13 +1,8 @@
 import React, { useState } from 'react'
 
-import clubService from '../services/clubService'
-
-
 const ClubsView = ({ clubs, members }) => {
 
-  const [clubMembers, setClubMembers] = useState([])
-
-  /* const rows = () => clubs.map( club => {
+  const rows = () => clubs.map( club => {
 
     const clubMembers = members.filter(member => member.clubId === club.id)
 
@@ -20,33 +15,13 @@ const ClubsView = ({ clubs, members }) => {
       {memberRows()}
       </div>
     )
-  }) */
-
-  const showClubMembers = () => clubs.map( club => {
-
-    clubService
-      .getClubMembers(club.id)
-      .then(response => {
-        setClubMembers(response)
-      }).catch(error => console.log(error))
-
-  const memberRows = () => clubMembers.map(member => <li>{member}</li>)
-
-    return(
-      <div>
-        <h3>{club.name}</h3>
-        {memberRows()}
-      </div>
-    )
-    
   })
 
   return(
 
     <div>
       <h2>Clubs and their members</h2>
-      {/* rows() */}
-      {showClubMembers()}
+      {rows()}
     </div>
 
   )
